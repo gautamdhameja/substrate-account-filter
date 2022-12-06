131a4685cee5e8b8361af171d063d8d4254234aa
akorchyn <artur.yurii.korchynskyi@gmail.com>
2022-12-06 15:58:31 +0200

    Futher fixes to make it working

--- src/lib.rs
+++ src/lib.rs
@@ -5,25 +5,27 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use frame_support::dispatch::DispatchInfo;
-use parity_scale_codec::{Decode, Encode};
-use scale_info::TypeInfo;
-use sp_runtime::{
-    traits::{DispatchInfoOf, Dispatchable, SignedExtension},
-    transaction_validity::{
-        InvalidTransaction, TransactionLongevity, TransactionValidity, TransactionValidityError,
-        ValidTransaction,
-    },
-};
-use sp_std::fmt::Debug;
-use sp_std::marker::PhantomData;
-use sp_std::prelude::*;
+pub use pallet::*;
 
 #[frame_support::pallet]
 pub mod pallet {
+
     use super::*;
+    use frame_support::dispatch::DispatchInfo;
     use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
     use frame_system::pallet_prelude::*;
+    use parity_scale_codec::{Decode, Encode};
+    use scale_info::TypeInfo;
+    use sp_runtime::{
+        traits::{DispatchInfoOf, Dispatchable, SignedExtension},
+        transaction_validity::{
+            InvalidTransaction, TransactionLongevity, TransactionValidity,
+            TransactionValidityError, ValidTransaction,
+        },
+    };
+    use sp_std::fmt::Debug;
+    use sp_std::marker::PhantomData;
+    use sp_std::prelude::*;
 
     #[pallet::config]
     pub trait Config: frame_system::Config {
@@ -115,90 +117,90 @@ pub mod pallet {
             }
         }
     }
-}
 
-/// The following section implements the `SignedExtension` trait
-/// for the `AllowAccount` type.
-/// `SignedExtension` is being used here to filter out the not allowed accounts
-/// when they try to send extrinsics to the runtime.
-/// Inside the `validate` function of the `SignedExtension` trait,
-/// we check if the sender (origin) of the extrinsic is part of the
-/// allow-list or not.
-/// The extrinsic will be rejected as invalid if the origin is not part
-/// of the allow-list.
-/// The validation happens at the transaction queue level,
-///  and the extrinsics are filtered out before they hit the pallet logic.
+    /// The following section implements the `SignedExtension` trait
+    /// for the `AllowAccount` type.
+    /// `SignedExtension` is being used here to filter out the not allowed accounts
+    /// when they try to send extrinsics to the runtime.
+    /// Inside the `validate` function of the `SignedExtension` trait,
+    /// we check if the sender (origin) of the extrinsic is part of the
+    /// allow-list or not.
+    /// The extrinsic will be rejected as invalid if the origin is not part
+    /// of the allow-list.
+    /// The validation happens at the transaction queue level,
+    ///  and the extrinsics are filtered out before they hit the pallet logic.
 
-/// The `AllowAccount` struct.
-#[derive(Encode, Decode, Clone, Eq, PartialEq, Default, TypeInfo)]
-pub struct AllowAccount<T: pallet::Config + Send + Sync>(PhantomData<T>);
+    /// The `AllowAccount` struct.
+    #[derive(Encode, Decode, Clone, Eq, PartialEq, Default, TypeInfo)]
+    pub struct AllowAccount<T: pallet::Config + Send + Sync>(PhantomData<T>);
 
-impl<T: pallet::Config + Send + Sync> AllowAccount<T> {
-    /// utility constructor. Used only in client/factory code.
-    pub fn new() -> Self {
-        Self(PhantomData)
-    }
-}
-
-/// Debug impl for the `AllowAccount` struct.
-impl<T: pallet::Config + Send + Sync> Debug for AllowAccount<T> {
-    #[cfg(feature = "std")]
-    fn fmt(&self, f: &mut sp_std::fmt::Formatter) -> sp_std::fmt::Result {
-        write!(f, "AllowAccount")
-    }
-
-    #[cfg(not(feature = "std"))]
-    fn fmt(&self, _: &mut sp_std::fmt::Formatter) -> sp_std::fmt::Result {
-        Ok(())
-    }
-}
-
-/// Implementation of the `SignedExtension` trait for the `AllowAccount` struct.
-impl<T: pallet::Config + Sync + Send + TypeInfo> SignedExtension for AllowAccount<T>
-where
-    T::RuntimeCall: Dispatchable<Info = DispatchInfo>,
-{
-    type AccountId = T::AccountId;
-    type Call = T::RuntimeCall;
-    type AdditionalSigned = ();
-    type Pre = ();
-    const IDENTIFIER: &'static str = "AllowAccount";
-
-    fn additional_signed(&self) -> sp_std::result::Result<(), TransactionValidityError> {
-        Ok(())
-    }
-
-    // Filter out the not allowed keys.
-    // If the key is in the allow-list, return a valid transaction,
-    // else return a custom error.
-    fn validate(
-        &self,
-        who: &Self::AccountId,
-        _call: &Self::Call,
-        info: &DispatchInfoOf<Self::Call>,
-        _len: usize,
-    ) -> TransactionValidity {
-        if <pallet::AllowedAccounts<T>>::contains_key(who) {
-            Ok(ValidTransaction {
-                priority: info.weight.ref_time(),
-                longevity: TransactionLongevity::max_value(),
-                propagate: true,
-                ..Default::default()
-            })
-        } else {
-            Err(InvalidTransaction::Call.into())
+    impl<T: pallet::Config + Send + Sync> AllowAccount<T> {
+        /// utility constructor. Used only in client/factory code.
+        pub fn new() -> Self {
+            Self(PhantomData)
         }
     }
 
-    fn pre_dispatch(
-        self,
-        who: &Self::AccountId,
-        call: &Self::Call,
-        info: &DispatchInfoOf<Self::Call>,
-        len: usize,
-    ) -> Result<Self::Pre, TransactionValidityError> {
-        self.validate(who, call, info, len)
-            .map(|_| ())
-            .map_err(Into::into)
+    /// Debug impl for the `AllowAccount` struct.
+    impl<T: pallet::Config + Send + Sync> Debug for AllowAccount<T> {
+        #[cfg(feature = "std")]
+        fn fmt(&self, f: &mut sp_std::fmt::Formatter) -> sp_std::fmt::Result {
+            write!(f, "AllowAccount")
+        }
+
+        #[cfg(not(feature = "std"))]
+        fn fmt(&self, _: &mut sp_std::fmt::Formatter) -> sp_std::fmt::Result {
+            Ok(())
+        }
+    }
+
+    /// Implementation of the `SignedExtension` trait for the `AllowAccount` struct.
+    impl<T: pallet::Config + Sync + Send + TypeInfo> SignedExtension for AllowAccount<T>
+    where
+        T::RuntimeCall: Dispatchable<Info = DispatchInfo>,
+    {
+        type AccountId = T::AccountId;
+        type Call = T::RuntimeCall;
+        type AdditionalSigned = ();
+        type Pre = ();
+        const IDENTIFIER: &'static str = "AllowAccount";
+
+        fn additional_signed(&self) -> sp_std::result::Result<(), TransactionValidityError> {
+            Ok(())
+        }
+
+        // Filter out the not allowed keys.
+        // If the key is in the allow-list, return a valid transaction,
+        // else return a custom error.
+        fn validate(
+            &self,
+            who: &Self::AccountId,
+            _call: &Self::Call,
+            info: &DispatchInfoOf<Self::Call>,
+            _len: usize,
+        ) -> TransactionValidity {
+            if <pallet::AllowedAccounts<T>>::contains_key(who) {
+                Ok(ValidTransaction {
+                    priority: info.weight.ref_time(),
+                    longevity: TransactionLongevity::max_value(),
+                    propagate: true,
+                    ..Default::default()
+                })
+            } else {
+                Err(InvalidTransaction::Call.into())
+            }
+        }
+
+        fn pre_dispatch(
+            self,
+            who: &Self::AccountId,
+            call: &Self::Call,
+            info: &DispatchInfoOf<Self::Call>,
+            len: usize,
+        ) -> Result<Self::Pre, TransactionValidityError> {
+            self.validate(who, call, info, len)
+                .map(|_| ())
+                .map_err(Into::into)
+        }
     }
 }
