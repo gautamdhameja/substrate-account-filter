1203da22418e73f775dacf17d37e7ee311d24c27
akorchyn <artur.yurii.korchynskyi@gmail.com>
2022-12-06 15:35:15 +0200

    Make it working with modern polkadot

--- src/lib.rs
+++ src/lib.rs
@@ -5,20 +5,19 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use frame_support::dispatch::DispatchInfo;
 use parity_scale_codec::{Decode, Encode};
-use frame_support::weights::DispatchInfo;
+use scale_info::TypeInfo;
 use sp_runtime::{
     traits::{DispatchInfoOf, Dispatchable, SignedExtension},
     transaction_validity::{
-        InvalidTransaction, TransactionLongevity, TransactionPriority, TransactionValidity,
-        TransactionValidityError, ValidTransaction,
+        InvalidTransaction, TransactionLongevity, TransactionValidity, TransactionValidityError,
+        ValidTransaction,
     },
 };
 use sp_std::fmt::Debug;
 use sp_std::marker::PhantomData;
 use sp_std::prelude::*;
-
-pub use pallet::*;
 
 #[frame_support::pallet]
 pub mod pallet {
@@ -26,10 +25,9 @@ pub mod pallet {
     use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
     use frame_system::pallet_prelude::*;
 
-    /// Configure the pallet by specifying the parameters and types on which it depends.
     #[pallet::config]
     pub trait Config: frame_system::Config {
-        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
     }
 
     #[pallet::pallet]
@@ -41,7 +39,6 @@ pub mod pallet {
     pub type AllowedAccounts<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, ()>;
 
     #[pallet::event]
-    #[pallet::metadata(T::AccountId = "AccountId")]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
         // When a new account is added to the allow-list.
@@ -91,7 +88,7 @@ pub mod pallet {
 
             Ok(().into())
         }
-        
+
         /// Remove an account from the allow-list.
         /// Can only be called by the root.
         #[pallet::weight(0)]
@@ -108,13 +105,13 @@ pub mod pallet {
             Ok(().into())
         }
     }
-}
 
-impl<T: Config> Pallet<T> {
-    fn initialize_allowed_accounts(allowed_accounts: &[(T::AccountId, ())]) {
-        if !allowed_accounts.is_empty() {
-            for (account, extrinsics) in allowed_accounts.iter() {
-                <AllowedAccounts<T>>::insert(account, extrinsics);
+    impl<T: Config> Pallet<T> {
+        fn initialize_allowed_accounts(allowed_accounts: &[(T::AccountId, ())]) {
+            if !allowed_accounts.is_empty() {
+                for (account, extrinsics) in allowed_accounts.iter() {
+                    <AllowedAccounts<T>>::insert(account, extrinsics);
+                }
             }
         }
     }
@@ -133,10 +130,10 @@ impl<T: Config> Pallet<T> {
 ///  and the extrinsics are filtered out before they hit the pallet logic.
 
 /// The `AllowAccount` struct.
-#[derive(Encode, Decode, Clone, Eq, PartialEq, Default)]
-pub struct AllowAccount<T: Config + Send + Sync>(PhantomData<T>);
+#[derive(Encode, Decode, Clone, Eq, PartialEq, Default, TypeInfo)]
+pub struct AllowAccount<T: pallet::Config + Send + Sync>(PhantomData<T>);
 
-impl<T: Config + Send + Sync> AllowAccount<T> {
+impl<T: pallet::Config + Send + Sync> AllowAccount<T> {
     /// utility constructor. Used only in client/factory code.
     pub fn new() -> Self {
         Self(PhantomData)
@@ -144,7 +141,7 @@ impl<T: Config + Send + Sync> AllowAccount<T> {
 }
 
 /// Debug impl for the `AllowAccount` struct.
-impl<T: Config + Send + Sync> Debug for AllowAccount<T> {
+impl<T: pallet::Config + Send + Sync> Debug for AllowAccount<T> {
     #[cfg(feature = "std")]
     fn fmt(&self, f: &mut sp_std::fmt::Formatter) -> sp_std::fmt::Result {
         write!(f, "AllowAccount")
@@ -157,12 +154,12 @@ impl<T: Config + Send + Sync> Debug for AllowAccount<T> {
 }
 
 /// Implementation of the `SignedExtension` trait for the `AllowAccount` struct.
-impl<T: Config + Send + Sync> SignedExtension for AllowAccount<T>
+impl<T: pallet::Config + Sync + Send + TypeInfo> SignedExtension for AllowAccount<T>
 where
-    T::Call: Dispatchable<Info = DispatchInfo>,
+    T::RuntimeCall: Dispatchable<Info = DispatchInfo>,
 {
     type AccountId = T::AccountId;
-    type Call = T::Call;
+    type Call = T::RuntimeCall;
     type AdditionalSigned = ();
     type Pre = ();
     const IDENTIFIER: &'static str = "AllowAccount";
@@ -181,9 +178,9 @@ where
         info: &DispatchInfoOf<Self::Call>,
         _len: usize,
     ) -> TransactionValidity {
-        if <AllowedAccounts<T>>::contains_key(who) {
+        if <pallet::AllowedAccounts<T>>::contains_key(who) {
             Ok(ValidTransaction {
-                priority: info.weight as TransactionPriority,
+                priority: info.weight.ref_time(),
                 longevity: TransactionLongevity::max_value(),
                 propagate: true,
                 ..Default::default()
@@ -191,5 +188,17 @@ where
         } else {
             Err(InvalidTransaction::Call.into())
         }
+    }
+
+    fn pre_dispatch(
+        self,
+        who: &Self::AccountId,
+        call: &Self::Call,
+        info: &DispatchInfoOf<Self::Call>,
+        len: usize,
+    ) -> Result<Self::Pre, TransactionValidityError> {
+        self.validate(who, call, info, len)
+            .map(|_| ())
+            .map_err(Into::into)
     }
 }
